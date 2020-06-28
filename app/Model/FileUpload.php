c7b02d5eb9ada3154f1cc6a9ec859b44084c9c32
Poy Refuerzo <popoy@yhmwhy.com>
2020-06-28 11:55:46 +0800

    cleaning

--- app/Model/FileUpload.php
+++ app/Model/FileUpload.php
@@ -3,28 +3,4 @@
 class FileUpload extends AppModel {
 
 
-    function import($filename) {
-        
-        $row = 1;
-        $arr = array();
-        if (($handle = fopen($filename, "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-             if($row != 1){ 
-                $arr[] =  array(
-                    "name" =>  $data[0],
-                    "email" =>  $data[1],
-                );
-            }
-            $row++;   
-              
-            }
-
-            $this->create();
-            $this->saveMany($arr);
-            fclose($handle);
-        }
-        
-    }
-
-
 }
